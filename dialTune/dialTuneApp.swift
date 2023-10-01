e665c5687377580488563b0663e25502d7cca2a0
Kudzai <kudzaishe@sleepiest.com>
2023-10-01 07:12:47 +0200

    [Added] Initial app commit

---
+++ dialTune/dialTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  dialTuneApp.swift
+//  dialTune
+//
+//  Created by KudzaisheMhou on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct dialTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
